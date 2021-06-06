08e8e45c80ece6389c54ceca3f151698471a8ed3
iinisialal <85207811+iinisialal@users.noreply.github.com>
2021-06-06 19:38:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'NAMA BOTMU'; 
+const     BotName     =     'ALBOT'   ;
 const instagram = 'LINK INSTAGRAMU'; 
 const telegram = 'LINK TELEMU'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
